ab65d4e3d432a7e7ee3c0f46a63ac981421caca7
Dmitry Nizhnikov <dmitrix_n@mail.ru>
2023-02-27 21:46:03 +0700

    added some simple things and requirements

--- web_app/__init__.py
+++ web_app/__init__.py
@@ -1,7 +1,7 @@
 from flask import Flask
 
 def create_app():
-    app = Flask(__name__)
+    app = Flask(__name__, instance_relative_config=True)
 
     from .main.routes import main
     app.register_blueprint(main)
