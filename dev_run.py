95b1e42d55ce576588d60eef8b2b75dd6172b92b
MityaiS <dmitrix_n@mail.ru>
2023-03-05 10:34:34 +0700

    added dev_run.py to run app in dev mode

---
+++ dev_run.py
@@ -0,0 +1,9 @@
+from web_app import create_app
+
+app = create_app({
+    "DEBUG": True,
+    "SECRET_KEY": "dev",
+})
+
+if __name__ == "__main__":
+    app.run()
--- web_app/__init__.py
+++ web_app/__init__.py
@@ -2,8 +2,13 @@ from flask import Flask
 from flask_login import LoginManager
 
 
-def create_app():
+def create_app(config_type=None):
     app = Flask(__name__, instance_relative_config=True)
+
+    if config_type:
+        app.config.update(config_type)
+    else:
+        app.config.from_pyfile("config.py")
 
     from .main.routes import main
     app.register_blueprint(main)
