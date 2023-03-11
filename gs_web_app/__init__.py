6b11f48b09e85ac0b0fae19b467566fa133e4c77
MityaiS <dmitrix_n@mail.ru>
2023-03-11 12:00:27 +0700

    made app work without config.py in instance folder

--- gs_web_app/__init__.py
+++ gs_web_app/__init__.py
@@ -4,10 +4,13 @@ from flask import Flask
 def create_app(config_type=None):
     app = Flask(__name__, instance_relative_config=True)
 
-    if config_type:
-        app.config.update(config_type)
+    if config_type is None:
+        try:
+            app.config.from_pyfile("config.py")
+        except FileNotFoundError:
+            pass
     else:
-        app.config.from_pyfile("config.py")
+        app.config.update(config_type)
 
     from .main.routes import main
     app.register_blueprint(main)
